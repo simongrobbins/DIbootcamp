988c12f3601a869c34e820ebe83957dc842403bf
simongrobbins <simgabrob@gmail.com>
2024-02-15 16:44:27 +0200

    correct miniproject

--- Week 3/Day 4/XPproject.html/script.js
+++ Week 3/Day 4/XPproject.html/script.js
@@ -1,70 +1,44 @@
-let color;
+
+let color = null;
 let mousedown = false;
 
 let body = document.getElementsByTagName("body")[0];
 let color_blocks = document.querySelectorAll("#sidebar > *");
-let fill_blocks = document.querySelectorAll("#main > *");
+let fill_blocks = document.querySelectorAll("#main");
 let clear_button = document.getElementsByTagName("button")[0];
 
+
+for (let i = 0; i < 1500; i++) {
+    const div = document.createElement('div');
+    main.appendChild(div);
+}
+
 clear_button.addEventListener("click", function(){
-    for (let fill_block of fill_blocks){
+    for (fill_block of fill_blocks){
         fill_block.style.backgroundColor = "white";
     }
 });
 
-const gridContainer = document.getElementById('gridContainer');
-let boardDivsCount = 25*60
-for (let i = 0; i < boardDivsCount; i++) {
-    const div = document.createElement('div');
-    gridContainer.appendChild(div);
-    div.setAttribute('class', 'grid div')
-  }
-
-
-  gridContainer.addEventListener("mousedown", function(){
+body.addEventListener("mousedown", function(){
     mousedown = true;
 })
 
-gridContainer.addEventListener("mouseup", function(){
+body.addEventListener("mouseup", function(){
     mousedown = false;
 })
 
 
-for (let color_block of color_blocks){
+for (color_block of color_blocks){
     color_block.addEventListener("click", function(event){
         color = event.target.style.backgroundColor;
     });
 }
 
-check()
-function check(){
-    console.log("hi");
-    for (let fill_block of fill_blocks){
-
-        fill_block.addEventListener("mousedown", function(event){
-            if (color){
-    
-                event.target.style.backgroundColor = color;
-                console.log(event.target);}
-        });
-        fill_block.addEventListener("mouseover", function(event){
-            if (mousedown && color != null) {event.target.style.backgroundColor = color;
-    }});
-    }
-
+for (fill_block of fill_blocks){
+    fill_block.addEventListener("mousedown", function(event){
+        if (color != null) event.target.style.backgroundColor = color;
+    });
+    fill_block.addEventListener("mouseover", function(event){
+        if (mousedown && color != null) event.target.style.backgroundColor = color;
+    });
 }
-// for (let fill_block of fill_blocks){
-
-//     fill_block.addEventListener("mousedown", function(event){
-//         if (color){
-
-//             event.target.style.backgroundColor = color;
-//             console.log(event.target);}
-//     });
-//     fill_block.addEventListener("mouseover", function(event){
-//         if (mousedown && color != null) {event.target.style.backgroundColor = color;
-// }});
-// }
-
-
-
